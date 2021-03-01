d44fdcef09d0a25028eae323c80662a66950d3a2
githubcaat <napaydin00@gmail.com>
2021-03-01 15:16:29 -0500

    first

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -4,7 +4,16 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.err.println("wordl");
+		System.out.println("wordl");
+		// Copy Github Repository URL
+		// Eclipse search git
+		// Clone a repository
+		// Paste repository url, type auth. next>next>finish
+		// Right click to projectName > Team > Share Project
+		// Select Repository > Click Finish
+		// Right click to projectName > Team > Commit
+		
+		
 
 	}
 
